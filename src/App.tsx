2fc91a825ca66444ba38142528e5ccaf2ee67e18
karthikprabha14 <48215542+karthikprabha14@users.noreply.github.com>
2025-04-03 13:30:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
             {loading ? (
                 <div>
                     <iframe
-                        src="https://sampleapp-rho.vercel.app/"
+                        src="https://mobile-pwa-one.dev-public.bbpd.io/"
                         width="100%"
                         height="100%"
                         title="Blackboard PWA"
